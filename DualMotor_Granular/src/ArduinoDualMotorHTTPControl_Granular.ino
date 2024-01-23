421849c19d793a145177f0aaab79937d0e04f789
Unobrasil <126009482+Unobrasil@users.noreply.github.com>
2024-01-23 10:55:46 -0800

    Add files via upload

---
+++ DualMotor_Granular/src/ArduinoDualMotorHTTPControl_Granular.ino
@@ -0,0 +1,167 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <Ethernet.h>
+
+ int motor1pin1 = 2;
+ int motor1pin2 = 3;
+ int motor1ENA = 9;
+
+ int motor2pin1 = 4;
+const int motor2pin2 = 5;
+const int motor2ENA = 6;
+
+void forwardL(int);
+void backwardL(int);
+void stopL();
+void forwardR(int);
+void backwardR(int);
+void stopR();
+void forwardAll(int);
+void backwardAll(int);
+void stopAll();
+
+struct Command {
+  const char* str;
+  void (*func)(int);
+};
+
+Command commands[] = {
+  { "forwardL", forwardL },
+  { "backwardL", backwardL },
+  { "stopL", stopL },
+  { "forwardR", forwardR },
+  { "backwardR", backwardR },
+  { "stopR", stopR },
+  { "forwardBoth", forwardAll },
+  { "backwardBoth", backwardAll },
+  { "stopBoth", stopAll }
+};
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; // Placeholder MAC Address
+IPAddress ip(192, 168, 1, 177); // Placeholder IP Address, should be unique in your network
+
+EthernetServer server(80); // Server will run on port 80
+
+void setup() {
+  pinMode(motor1pin1, OUTPUT);
+  pinMode(motor1pin2, OUTPUT);
+  pinMode(motor1ENA, OUTPUT);
+
+  pinMode(motor2pin1, OUTPUT);
+  pinMode(motor2pin2, OUTPUT);
+  pinMode(motor2ENA, OUTPUT);
+
+  Serial.begin(9600);
+
+  Ethernet.begin(mac, ip);
+  server.begin();
+}
+
+void loop() {
+  EthernetClient client = server.available();
+  if (client) {
+    String currentLine = ""; 
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();  
+        if (c == '\n') {
+          if (currentLine.length() == 0) {
+            break;
+          } else {
+            if (currentLine.startsWith("GET")) {
+              executeMotorCommand(currentLine.c_str());
+              client.println("HTTP/1.1 200 OK");
+              client.println("Content-Type: text/html");
+              client.println("Connection: close");
+              client.stop();
+            }
+            currentLine = "";
+          }
+        } else if (c != '\r') {
+          currentLine += c;
+        }
+      }
+    }
+    delay(1);
+    client.stop();
+  }
+}
+
+void executeMotorCommand(const char* command) {
+  if (strncmp(command, "GET /", 5) != 0) {
+    return;  // not a GET request
+  }
+
+  // Start parsing after "GET /"
+  command += 5;
+
+  // Find the end of the command word
+  char* endOfCommand = strpbrk(command, " ?");
+  if (!endOfCommand) {
+    return;  // ill-formed request
+  }
+
+  // Find the speed parameter
+  int speed = 0; 
+  char* speedPtr = strstr(command, "?speed=");
+  if (speedPtr) {
+    sscanf(speedPtr, "?speed=%d", &speed);
+  }
+
+  // Function to parse the client's request and execute the corresponding command
+  int commandLength = endOfCommand - command;
+  for (unsigned int i = 0; i < sizeof(commands) / sizeof(Command); ++i) {
+    if (strncmp(command, commands[i].str, commandLength) == 0 
+         && commandLength == strlen(commands[i].str)) { // make sure lengths are equal
+      commands[i].func(speed);
+      return;
+    }
+  }
+}
+
+void forwardL(int speed) {
+  digitalWrite(motor1pin1, HIGH);
+  digitalWrite(motor1pin2, LOW);
+  analogWrite(motor1ENA, speed);
+}
+
+void backwardL(int speed) {
+  digitalWrite(motor1pin1, LOW);
+  digitalWrite(motor1pin2, HIGH);
+  analogWrite(motor1ENA, speed);
+}
+
+void stopL() {
+  analogWrite(motor1ENA, 0);
+}
+
+void forwardR(int speed) {
+  digitalWrite(motor2pin1, HIGH);
+  digitalWrite(motor2pin2, LOW);
+  analogWrite(motor2ENA, speed);
+}
+
+void backwardR(int speed) {
+  digitalWrite(motor2pin1, LOW);
+  digitalWrite(motor2pin2, HIGH);
+  analogWrite(motor2ENA, speed);
+}
+
+void stopR() {
+  analogWrite(motor2ENA, 0);
+}
+
+void forwardAll(int speed){
+  forwardL(speed);
+  forwardR(speed);
+}
+
+void backwardAll(int speed){
+  backwardL(speed);
+  backwardR(speed);
+}
+
+void stopAll(){
+  stopL();
+  stopR();
+}
